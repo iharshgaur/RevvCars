dfcc4d7f57bac783a29c4d07813e8b600e86e1a5
ManishRaghavan <manish.mahendran9499@gmail.com>
2021-03-09 23:57:06 +0530

    minor change for .env

--- src/Pages/subscription/MainPart/SubscriptionPage.jsx
+++ src/Pages/subscription/MainPart/SubscriptionPage.jsx
@@ -3,7 +3,7 @@ import style from "./SubscriptionPage.module.css"
 export const SubscriptionPage = () => {
     return (
         <div>
-            <h1>Hello world</h1>
+            <div className={style.mainContainer}></div>
         </div>
     )
 }
