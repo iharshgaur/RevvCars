5882a6a870dcf67de97bca7de29314bcb415b6e0
ManishRaghavan <manish.mahendran9499@gmail.com>
2021-03-09 23:54:22 +0530

    addind the title logo and change the title in addition to that created files for subscription page

---
+++ src/Pages/subscription/MainPart/SubscriptionPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from "./SubscriptionPage.module.css"
+export const SubscriptionPage = () => {
+    return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
