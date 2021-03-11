10ce7ded3bf62666a7cf0e4c88ba54f3cfd02a41
ManishRaghavan <manish.mahendran9499@gmail.com>
2021-03-12 00:30:41 +0530

    subscription payment page is done

---
+++ src/Pages/payment/FinalPayment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router'
+
+export const FinalPayment = () => {
+    const l = useParams()
+    
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
