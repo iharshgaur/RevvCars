d385b806d65b70a77ee606e601c32d288fbc105f
Harsh Gaur <harshgaur997@gmail.com>
2021-03-13 00:45:53 +0530

    profile content

--- src/Components/Profile/ProfileContent.jsx
+++ src/Components/Profile/ProfileContent.jsx
@@ -3,6 +3,7 @@ import styles from "./ProfileContent.module.css";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 import { logoutUser } from "../../Redux/Auth/action";
 const useStyles = makeStyles({});
 
@@ -10,8 +11,6 @@ function ProfileContent() {
   const { email, username, cars_subscribed, cars_booked } = useSelector(
     (state) => state.auth.currentUser
   );
-
-  React.useEffect(() => {}, []);
 
   const classes = useStyles();
   const dispatch = useDispatch();
